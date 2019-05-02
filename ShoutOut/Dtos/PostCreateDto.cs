0e36e712e8b47ef7a1a8caf663d62b82a7c460ce
Lourenco, Cesar <E84431@hugoboss.com>
2019-05-02 14:13:39 +0100

    - Posts controller created and dto classes added;

---
+++ ShoutOut/Dtos/PostCreateDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoutOut.Dtos
+{
+	public class PostCreateDto
+	{
+		private string authorId;
+		private string author;
+		private string title;
+		private string message;
+
+		public string AuthorId
+		{
+			get => authorId;
+			set => authorId = value;
+		}
+
+		public string Author
+		{
+			get => author;
+			set => author = value;
+		}
+
+		public string Title
+		{
+			get => title;
+			set => title = value;
+		}
+
+		public string Message
+		{
+			get => message;
+			set => message = value;
+		}
+	}
+}
---
+++ ShoutOut/Dtos/PostUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoutOut.Dtos
+{
+	public class PostUpdateDto
+	{
+		private string title;
+		private string message;
+
+		public string Title
+		{
+			get => title;
+			set => title = value;
+		}
+
+		public string Message
+		{
+			get => message;
+			set => message = value;
+		}
+	}
+}
