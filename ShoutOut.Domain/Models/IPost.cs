12e691f98336d099520385bc103a8b37fe0de967
Lourenco, Cesar <E84431@hugoboss.com>
2019-05-02 14:14:11 +0100

    - Domain project created and model classes added;

---
+++ ShoutOut.Domain/Models/IPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoutOut.Domain.Models
+{
+	public interface IPost
+	{
+		string Id { get; set; }
+
+		string AuthorId { get; set; }
+
+		string Author { get; set; }
+
+		string Title { get; set; }
+
+		string Message { get; set; }
+
+		DateTime Created { get; set; }
+
+		DateTime Updated { get; set; }
+	}
+}
---
+++ ShoutOut.Domain/Models/Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ShoutOut.Domain.Models
+{
+	public class Post : IPost
+	{
+		private string id;
+		private string authorId;
+		private string author;
+		private string title;
+		private string message;
+		private DateTime created;
+		private DateTime updated;
+
+		public string Id
+		{
+			get => id;
+			set => id = value;
+
+		}
+
+		public string AuthorId
+		{
+			get => authorId;
+			set => authorId = value;
+		}
+
+		public string Author
+		{
+			get => author;
+			set => author = value;
+		}
+
+		public string Title
+		{
+			get => title;
+			set => title = value;
+		}
+
+		public string Message
+		{
+			get => message;
+			set => message = value;
+		}
+
+		public DateTime Created
+		{
+			get => created;
+			set => created = value;
+		}
+
+		public DateTime Updated
+		{
+			get => updated;
+			set => updated = value;
+		}
+	}
+}
