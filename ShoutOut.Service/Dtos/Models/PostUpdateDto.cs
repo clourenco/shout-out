ec4b39797cfb116523d5200d7df1c6a38eb5d8db
Lourenco, Cesar <E84431@hugoboss.com>
2019-05-05 15:52:24 +0100

    - Started to refactor post controller in order to use custom responses; - Added validation to post domain model; - Deleted folder Handlers from ShoutOut.Domain; - Minor clean-up in Startup.cs; - Added folder Responses to ShoutOut.Service.Tests project;
    Revert "Revert "- Added data annotations in the dto's;""
    
    This reverts commit c6c795c81cc9e05af74e37eecea00e1f3e334969.

--- ShoutOut.Service/Dtos/Models/PostUpdateDto.cs
+++ ShoutOut.Service/Dtos/Models/PostUpdateDto.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ShoutOut.Service.Dtos.Models
 {
 	public class PostUpdateDto
 	{
+		[Required]
+		[StringLength(100, ErrorMessage = "The title cannot be empty and should have a maximum of 100 characters long.", MinimumLength = 1)]
 		public string Title { get; set; }
 
+		[Required]
+		[StringLength(1000, ErrorMessage = "The message cannot be empty and should have a maximum of 1000 characters long.", MinimumLength = 1)]
 		public string Message { get; set; }
 	}
 }
